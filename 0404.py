a27755c6b3283c08fbc04a401c2a95c14f8ec97d
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-04-04 11:05:06 -0700

    ver2025.0404.4

--- 0404.py
+++ 0404.py
@@ -1,5 +1,4 @@
 import turtle
-import random
 
 wn = turtle.Screen()
 wn.setup(width=600, height=600)
@@ -31,12 +30,35 @@ while i < len(x_list):
 sigma_x = 0
 sigma_y = 0
 sigma_xx = 0
+sigma_xy = 0
 i = 0
 
 while i < len(x_list):
-    sigma_x = sigma_x + x_list[i]
-    sigma_y = sigma_y + y_list[i]
-    sigma_xx = sigma_xx + x_list[i] * x_list[i]
+    sigma_x = sigma_x + x_list[i] #x加總
+    sigma_y = sigma_y + y_list[i] #y加總
+    sigma_xx = sigma_xx + x_list[i] * x_list[i] #x加總的平方
+    sigmna_xy = sigma_xy + y_list[i] * y_list[i]
     i = i + 1
 
 print(sigma_x, sigma_y, sigma_xx)
+
+
+#-------
+
+n = len(x_list)
+
+xbar = sigma_x / n
+ybar = sigma_y / n
+
+upfloor = (sigma_xy) - (n * xbar * ybar)
+dowfloor = (sigma_xx) - n * (xbar * xbar)
+
+answer_a = upfloor / dowfloor
+answer_b = ybar - (answer_a * xbar)
+print(answer_a, answer_b)
+
+pen.goto(min(x_list) * factor, (answer_a * min(x_list) + answer_b) * factor)
+pen.pendown()
+pen.goto(max(x_list) * factor, (answer_a * max(x_list) + answer_b) * factor)
+
+wn.exitonclick()
