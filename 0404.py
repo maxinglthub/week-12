de02fb984591d998e93152ef8e3938ec6e31114e
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-04-04 10:07:05 -0700

    ver2025.0404.2

--- 0404.py
+++ 0404.py
@@ -15,12 +15,8 @@ pen.pendown()
 pen.goto(0, -300)
 pen.penup()
 
-x_list = []
-y_list = []
-
-while len(x_list) < 10:
-    x_list.append(random.randint(-5, 5))
-    y_list.append(random.randint(-5, 5))
+x_list = [1, 3, 4, 5]
+y_list = [2, 4, 6, 8]
 
 print(x_list, y_list)
 
@@ -32,4 +28,10 @@ while i < len(x_list):
     pen.stamp()
     i = i + 1
 
-#test
+sigma_x = 0
+i = 0
+
+while i < len(x_list):
+    sigma_x = sigma_x + x_list[i]
+    i = i + 1
+print(sigma_x)
